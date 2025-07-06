309f094bc42feddeef6fe7c8b0c24b221e16d3b3
Edwin Atuncar <edwinatuncar@gmail.com>
2025-07-05 22:28:07 -0500

    update files

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.xresolve.lockshortcut"
         minSdk = 35
         targetSdk = 35
-        versionCode = 3
-        versionName = "1.0.10"
+        versionCode = 4
+        versionName = "1.0.11"
     }
 
     compileOptions {
--- app/src/main/java/com/xresolve/lockshortcut/MainActivity.kt
+++ app/src/main/java/com/xresolve/lockshortcut/MainActivity.kt
@@ -5,23 +5,24 @@ import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val devicePolicyManager = getSystemService(Context.DEVICE_POLICY_SERVICE) as DevicePolicyManager
+        val dpm = getSystemService(Context.DEVICE_POLICY_SERVICE) as DevicePolicyManager
         val adminComponent = ComponentName(this, LockDeviceAdminReceiver::class.java)
 
-        if (devicePolicyManager.isAdminActive(adminComponent)) {
-            devicePolicyManager.lockNow()
+        if (dpm.isAdminActive(adminComponent)) {
+            dpm.lockNow()
             finish()
         } else {
- 
+            Toast.makeText(this, "Please activate device admin", Toast.LENGTH_LONG).show()
             val intent = Intent(DevicePolicyManager.ACTION_ADD_DEVICE_ADMIN).apply {
                 putExtra(DevicePolicyManager.EXTRA_DEVICE_ADMIN, adminComponent)
-                putExtra(DevicePolicyManager.EXTRA_ADD_EXPLANATION, "Lock button requires Device Admin permission to work.")
+                putExtra(DevicePolicyManager.EXTRA_ADD_EXPLANATION, "Required to lock screen")
             }
             startActivity(intent)
         }
