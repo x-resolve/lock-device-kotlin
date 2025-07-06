7f2ec6b0546109ade14df125c36411520179bd28
Edwin Atuncar <edwinatuncar@gmail.com>
2025-07-06 00:05:44 -0500

    update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.xresolve.lockshortcut"
         minSdk = 35
         targetSdk = 35
-        versionCode = 11
-        versionName = "1.0.18"
+        versionCode = 12
+        versionName = "1.0.19"
     }
 
     compileOptions {
